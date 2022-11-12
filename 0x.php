ef8f1e7d5091a60266c480737801bd050415f3cd
sachiihaxor <106025611+sachiihaxor@users.noreply.github.com>
2022-11-12 22:00:08 +0700

    Create 0x.php

---
+++ 0x.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Wh00ps! - Stoupid!</title>
+	<link rel="shortcut icon" href="https://pngimg.com/uploads/devil/devil_PNG50.png" sizes="16x16">
+	<meta name="description" content="- Learn Exploitasi Hacking to It's Money -"/>
+
+</head>
+<body>
+<center>
+	<img src="https://pngimg.com/uploads/devil/devil_PNG50.png" width="200">
+	<br>
+	<font color="black">
+	<?php echo php_uname();?>
+	<br><br>
+	- Learn Exploitasi Hacking to It's Money -
+	<br><br>
+	Greetz : -<font color="red"> Stoupid!</font> - Mc'Sl0vv - JakSec166 - Atengg337 - FellGans - K4PUYU4K CITER
+	<br><br>
+	Copyright &copy; <a href="https://hackermind.id/"><font color="red">hackermind.id</font></a> <?php echo date('Y');?>
+<audio id="audio" autoplay="" loop>
+<source src="https://www.mboxdrive.com/eas.mp3">
+</audio>
+</center>
+</body>
+</html>
